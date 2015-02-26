8c670d8dae8d679402145c702f7356807324acd8
nickolas360 <nickolas360@users.noreply.github.com>
2015-02-26 00:02:13 -0800

    Update pyrcb Fix LGPL header

--- hamelin-py/bin/lib/pyrcb.py
+++ hamelin-py/bin/lib/pyrcb.py
@@ -1,9 +1,9 @@
 # Copyright (C) 2015 nickolas360 (https://github.com/nickolas360)
 #
 # This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Lesser General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
+# it under the terms of the GNU Lesser General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
 #
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
